d80c4088b7569703c033a3ae839679661100cf7e
DipDM <161241849+DipDM@users.noreply.github.com>
2024-09-06 18:51:55 +0530

    modify Putof productimage

--- obj/Debug/net8.0/CoolCBackEnd.AssemblyInfo.cs
+++ obj/Debug/net8.0/CoolCBackEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoolCBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a7b5bac218258d3e9137a921c249fe4ff9824e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6196789c7aa74037c65d19d090b237282a8e09da")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoolCBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoolCBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
